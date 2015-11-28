660089b84d0a8aab1c28b7799b17f7ac3010e9d3
Karl Bowden <karl@karlbowden.com>
2015-11-28 15:37:43 +1100

    Fix #3 and update dependencies
    Signed-off-by: Karl Bowden <karl@karlbowden.com>

--- tasks/fontgen.js
+++ tasks/fontgen.js
@@ -2,8 +2,10 @@
  * grunt-fontgen
  * https://github.com/agentk/grunt-fontgen
  *
- * Copyright (c) 2014 Karl Bowden
- * Licensed under the MIT license.
+ * Copyright 2015 Karl Bowden
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
 'use strict';
@@ -18,7 +20,8 @@ module.exports = function(grunt) {
 
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
-            path_prefix: ''
+            path_prefix: '',
+            stylesheet: 'fonts.css'
         });
 
         // Iterate over all specified file groups.
@@ -37,6 +40,7 @@ module.exports = function(grunt) {
                 fontfacegen({
                     source: filepath,
                     dest: f.dest,
+                    css: options.stylesheet,
                     css_fontpath: options.path_prefix
                 })
 
